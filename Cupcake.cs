041bda29b3aab2eb0211c242ba4cbd9c69fc2618
jbaxter <jbaxter@916-IAM-139-15T>
2023-04-10 13:29:54 -0500

    Add project files.

---
+++ Cupcake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace DataBindingDemo2023
+{
+    public class Cupcake
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public BitmapImage Picture { get; set; }
+
+        public Cupcake(string _name, string _description, string _path)
+        {
+            Name = _name;
+            Description = _description;
+            Picture = new BitmapImage(new Uri(_path, UriKind.Relative));
+        }
+        public Cupcake() { }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingDemo2023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Cupcake[] cupcakes =
+        {
+            new Cupcake("Chocolate Explosion", "An awesome...", "chocolate.png"),
+            new Cupcake("Vanilla Something", "A description here...", "vanilla.png"),
+            new Cupcake("Blueberry Bonanza", "description...", "blueberry.png")
+        };
+        int index = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = cupcakes[index];
+        }
+
+        private void NaviagationButton_Click(object sender, RoutedEventArgs e)
+        {
+            index++;
+            if (index >= cupcakes.Length)
+                index = 0;
+            DataContext = cupcakes[index];
+
+        }
+    }
+}
